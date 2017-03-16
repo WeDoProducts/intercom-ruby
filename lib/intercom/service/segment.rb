6c49a88682b9bb4a5c7e7b7268f5c56511fad28b
Murad Yusufov <flood4life@gmail.com>
2017-03-16 13:01:32 +0300

    Add FindAll operation to Segment

--- lib/intercom/service/segment.rb
+++ lib/intercom/service/segment.rb
@@ -1,12 +1,14 @@
 require 'intercom/service/base_service'
 require 'intercom/api_operations/list'
 require 'intercom/api_operations/find'
+require 'intercom/api_operations/find_all'
 
 module Intercom
   module Service
     class Segment < BaseService
       include ApiOperations::List
       include ApiOperations::Find
+      include ApiOperations::FindAll
 
       def collection_class
         Intercom::Segment
